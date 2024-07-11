8c0c21e21ff2edb5bf4af4b5cc57bebb6f6ec69d
Turbold <turbolddev@gmail.com>
2024-07-11 19:28:44 +0800

    fixed env error

--- src/config.ts
+++ src/config.ts
@@ -2,12 +2,12 @@ import { DynamoDB } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 
 const awsCredentials = {
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
+  accessKeyId: process.env.MY_AWS_ACCESS_KEY,
+  secretAccessKey: process.env.MY_AWS_SECRET_KEY,
 };
-console.log(process.env.AWS_ACCESS_KEY);
+console.log(process.env.MY_AWS_ACCESS_KEY);
 const dynamoConfig = {
-  region: process.env.AWS_REGION,
+  region: process.env.MY_AWS_REGION,
   credentials: awsCredentials,
 } as {
   credentials: {
